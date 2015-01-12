20facd3edbbef95a216651b203ab3c410b7046a4
Duane Jeffers <duane@duanejeffers.com>
2015-01-12 04:59:56 -0600

    Added initial index.php and htaccess file

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+	/****
+	 * /r/EVEX Voting Application
+	 * Written By Duane Jeffers (midniteslayr) <duane@duanejeffers.com>
+	 *
+	 * See LICENSE for license information.
+	 ****/
+
+	require_once('../vendor/autoload.php');
+
+	ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+	$app = new \Slim\Slim(array(
+		'view' => new \TJC\View\Layout(),
+		'templates.path' => '../app/templates',
+		'mode' => APPLICATION_MODE
+	));
+
+	$app->view->setLayout('layout/layout.php');
+
+	$app->view->appendJavascriptFile('/components/jquery/dist/jquery.min.js')
+			  ->appendJavascriptFile('/components/bootstrap/dist/js/bootstrap.min.js')
+			  ->appendJavascriptFile('/js/application.js');
+
+	$app->view->appendStylesheet('/components/bootstrap/dist/css/bootstrap.min.css')
+			  ->appendStylesheet('/components/fontawesome/css/font-awesome.min.css')
+			  ->appendStylesheet('/css/application.css');
+	
+	$app->get('/', function() use($app) {
+		$app->render('main/index.php', array());
+	});
+
+	$app->run();
